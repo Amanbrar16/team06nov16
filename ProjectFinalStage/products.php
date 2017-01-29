e80890ca769dc994e0e1eab1a0d7472ee7630afe
jc353332 <harpreetkaur8@my.jcu.edu.au>
2017-01-29 17:39:01 +1000

    Add files via upload

---
+++ ProjectFinalStage/products.php
@@ -0,0 +1,77 @@
+<?php include_once('header.html');?>
+<h2 class="text-center">RECOMMENDED PRODUCTS</h2>
+<hr>
+<div class="container">
+  <div class="row text-center">
+    <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+      <div class="thumbnail"> <img src="images/3.jpg" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          <p></p>
+          <p>&nbsp;</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+      <div class="thumbnail"> <img src="images/images.jpg" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          
+          <p>&nbsp;</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+      <div class="thumbnail"> <img src="images/60b14b95bc8a5de4aa7ce8135d3c655c.jpg" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          <p></p>
+          <p>&nbsp;</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6 hidden-lg hidden-md hidden-sm">
+      <div class="thumbnail"> <img src="images/400X200.gif" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          <p></p>
+          <p><a href="#" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> </a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="row text-center hidden-xs">
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="thumbnail"> <img src="images/download (1).jpg" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          <p></p>
+          <p>&nbsp;</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="thumbnail"> <img src="images/download.jpg" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          <p></p>
+          <p>&nbsp;</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div class="thumbnail"> <img src="images/3.jpg" alt="Thumbnail Image 1" class="img-responsive">
+        <div class="caption">
+          <h3>Product</h3>
+          <p></p>
+          <p><a href="#" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Add to Cart</a> </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+    </div>
+  </div>
+</div>
+<?php include_once('footer.html');?>
