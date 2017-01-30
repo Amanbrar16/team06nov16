37d733601fda8d9e085aa9dcd72947fdcd747ffc
gaganbishnoi <gagandeep2@my.jcu.edu.au>
2017-01-30 10:19:03 +1000

    Add files via upload

---
+++ ProjectFinalStage/index.php
@@ -0,0 +1,63 @@
+<?php include('header.html') ?>
+<div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        
+      </div>
+</div>
+    <hr>
+  </div>
+<div class="container">
+  <div class="row">
+  
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+      <div class="row">
+        <div class="col-lg-2 col-md-3 col-sm-3 col-xs-2"><img class="img-circle" alt="Free Shipping" src="images/40X40.gif"></div>
+        <div class="col-lg-6 col-md-9 col-sm-9 col-xs-9">
+          <h4>Free Returns</h4>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+      <div class="row">
+        <div class="col-lg-2 col-md-3 col-sm-3 col-xs-2"><img class="img-circle" alt="Free Shipping" src="images/40X40.gif"></div>
+        <div class="col-lg-6 col-md-9 col-sm-9 col-xs-9">
+          <h4>Low Prices</h4>
+        </div>
+      </div>
+    </div>
+    <p>.  </p>
+  </div>
+</div>
+<hr>
+<div class="container">
+  <div class="row text-center">
+    <div class="col-sm-6 col-md-4 col-lg-4 col-xs-6">
+      <div class="thumbnail"> <img src="images/4.jpg" alt="Thumbnail Image 1" class="img-responsive" id="thumbnail-pic">
+        <div class="caption">
+          <h3>Product</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing.        </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-4 col-xs-6">
+      <div class="thumbnail"> <img src="images/6.jpg" alt="Thumbnail Image 1" width="239" height="347" class="img-responsive" id="thumbnail-pic" >
+        <div class="caption"> 
+          <h3>Product</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing.        </p>
+        </div>
+      </div>
+    </div>
+   
+   
+  </div>
+  
+ 
+  
+  
+  
+   
+  </div>
+</div>
+<?php include('footer.html') ?>
+ 
